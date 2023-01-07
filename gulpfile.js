d48b156c3cf16c7d78d87a769e40f46806657416
Luhao <yangluhao123@gmail.com>
2023-01-07 20:54:18 +1300

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,48 @@
-'use strict';
-
-// Gulp and node
-const gulp = require('gulp');
-
-
-// Image Generation
-const responsive = require('gulp-responsive');
-const rename = require('gulp-rename');
+const { dest, series, src } = require('gulp');
+const through2 = require('through2');
 const imagemin = require('gulp-imagemin');
+const sharp = require('sharp');
 
 
-// Images
-gulp.task('img', function() {
-  return gulp.src('_img/posts/*.{png,jpg}')
-    .pipe(responsive({
-      // For all the images in the folder
-      '*': [{
-        width: 230,
-        rename: {suffix: '_placehold'},
-      }, {
-        // thubmnail
-        width: 535,
-        rename: { suffix: '_thumb' },
-      }, {
-        // thumbnail @2x
-        width: 535 * 2,
-        rename: { suffix: '_thumb@2x' },
-      }, {
-        width: 575,
-        rename: { suffix: '_xs'}
-      }, {
-        width: 767,
-        rename: {suffix: '_sm'}
-      }, {
-        width: 991,
-        rename: { suffix: '_md' }
-      }, {
-        width: 1999,
-        rename: { suffix: '_lg' }
-      }, {
-        // max-width hero
-        width: 1920,
-      }],
-    }, {
-      progressive: true,
-      withMetadata: true,
-      errorOnEnlargement: false,
-    }))
-    .pipe(imagemin())
-    .pipe(gulp.dest('assets/img/posts/'));
-});
+function clean(cb) {
+  console.log("clean", cb)
+  cb();
+}
+
+function resize() {
+  return through2.obj(function(file, _, cb) {
+    const outputFile = `${file.dirname}/${file.stem}-test${file.extname}`
+    console.log(outputFile)
+    // to do: use sharp to resize images
+    sharp(file.contents)
+    .resize(320, 240)
+    .toFile(outputFile, (err, info) => { 
+      console.log(info)
+      cb(null, file);
+     });
+    
+  })
+}
+
+function printInfo() {
+  return through2.obj(function(file, _, cb) {
+    console.log(file.path)
+     cb(null, file);
+  })
+}
+
+
+function processImages() {
+  console.log("processImages")
+  return src('assets/img/posts/*.{png,jpg}')
+    .pipe(resize())
+// .pipe(imagemin())
+    .pipe(printInfo())
+    .pipe(dest('assets/img/tests/'));
+}
+
+exports.processImages = processImages;
+exports.default = series(clean, processImages);
+
 
 
